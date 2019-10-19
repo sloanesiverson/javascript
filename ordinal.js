20dcf5ce2e1fb884db6997d5c1e4bb3e103ebb7f
sloanesiverson <sloanems@aol.com>
2019-10-19 21:48:14 +0000

    ordinal

--- ordinal.js
+++ ordinal.js
@@ -3,7 +3,25 @@ var readlineSync = require('readline-sync');
 var ordinal = function(digit) {
   var digitNum = Number(digit);
   var lastDigit = digitNum % 10;
-  return lastDigit;
+  var lastTwoDigits = digitNum % 100;
+  if (lastTwoDigits >10 && lastTwoDigits < 20) {
+    return digit + "th";
+  }
+
+  else if (lastDigit === 1) {
+    return digit + "st";
+  }
+  
+else if (lastDigit === 2) {
+  return digit + "nd";
+}
+else if (lastDigit === 3) {
+  return digit + "rd";
+}
+else {
+  return digit + "th";
+}
+return lastDigit;
 }
 
 
